c2d4c7fdfcad9af4f623e10044488eb5067684da
DANISH <danishmubashar81@gmail.com>
2025-09-12 07:35:50 +0500

    this is main branch commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# this is danish branch so 
-print("danish_saim branch")
-# 1st i create a simple code for clear concept of branch this is  main branch
-print("Hello, welcome to the ML Git demo repository!")
+# this is main branch
+
+
+print("main branch hai ye ab hum merge krte han danish ma main ko")
